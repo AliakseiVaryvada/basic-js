f944b95eab01a6332e39342828abcb7b37c7c899
Aliaksei <alv96@bk.ru>
2020-09-28 14:43:48 +0300

    feat: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (Array.isArray(members)) {
+        return (Array.from(
+            (members.filter(word => typeof word === 'string')),
+                x => x.replace(/\s/g, '')
+                    .substring(0, 1)
+                    .toUpperCase())
+            .sort())
+            .join('');
+    } else {
+        return false;
+    }
 };
