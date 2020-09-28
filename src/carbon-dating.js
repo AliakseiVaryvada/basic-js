1ccb4b9e64a0061139e58399537628b5b6121a58
Aliaksei <alv96@bk.ru>
2020-09-28 14:15:43 +0300

    feat: carbon-dating and count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,11 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    const activity = parseFloat(sampleActivity);
+    if (typeof sampleActivity !== 'string' || isNaN(activity) || activity > MODERN_ACTIVITY || activity <= 0) {
+        return false;
+    }
+    const ln2 = 0.693;
+    return Math.ceil(Math.log(MODERN_ACTIVITY/activity)/(ln2/HALF_LIFE_PERIOD));
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+    let count = 0;
+    if(matrix.length > 0) {
+        matrix.reduce((acc, val) => acc.concat(val)).forEach((item) => {
+            if (item === '^^') {
+                count = count + 1
+            }
+        })
+    }
+    return count;
 };
